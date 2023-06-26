1cf6764fc8933771a6d72ee795e75f14fd96274f
ja <ja@zdb.es>
2023-06-26 22:57:25 +0200

    primera prueba

--- pfs/matriz/views.py
+++ pfs/matriz/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+
+    return HttpResponse ("Inicio")
+
+def matriz(request):
+
+    return HttpResponse ("Matriz")
+
+def historico(request):
+
+    return HttpResponse ("Historico")
--- pfs/pfs/urls.py
+++ pfs/pfs/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from matriz import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('matriz', views.matriz, name="matriz"),
+    path('historico', views.historico, name="historico"),
 ]
