6a8027a581e909d37920e8b9512f6783490dc9ad
ja <ja@zdb.es>
2023-07-22 22:37:22 +0200

    retoques finales de la web

--- pfs/pfs/urls.py
+++ pfs/pfs/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('matriz/', include('matriz.urls')),
+    path('', include('matriz.urls')),
 ]
